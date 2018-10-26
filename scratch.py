f8e2f16a71982d11729297acc7e6b704f8922286
Oscar Byrne <oscar.byrne@gmail.com>
2018-10-26 16:27:30 -0700

    add Key class for finding correlations

---
+++ scratch.py
@@ -0,0 +1,39 @@
+from enum import Enum, auto
+
+import pandas as pd
+
+
+CHROMATIC_CARDINALITY = 12
+
+def pitch_classes(pitches):
+    pitches = pd.Series(pitches)
+    return pitches.mod(
+        CHROMATIC_CARDINALITY
+    )
+
+class Key():
+
+    class Color(Enum):
+        MAJOR = auto()
+        MINOR = auto()
+
+    profiles = {
+        Color.MAJOR: pd.Series([6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]),
+        Color.MINOR: pd.Series([6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]),
+    }
+
+    def __init__(self, tonic, color = Color.MAJOR):
+        self.tonic = tonic
+        self.color = color
+
+    @property
+    def profile(self):
+        return self.profiles[self.color]
+
+    def correlation(self, pitches):
+        return self.profile.corr(
+            pitch_classes(pitches).value_counts()
+        )
+
+    def __repr__(self):
+        return f'Key({self.tonic}, {self.color})'
